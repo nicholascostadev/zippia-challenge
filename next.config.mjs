84904524299702770e486133ad1cdea9f929ebb2
Nicholas Costa <nicholascostadev@gmail.com>
2023-01-11 15:53:21 -0300

    nextjs: enable all image urls with https

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,12 @@ const config = {
     defaultLocale: "en",
   },
   images: {
-    domains: ['placeimg.com', 'logo.clearbit.com']
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   }
 };
 export default config;
